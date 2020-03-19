385df8adf22a4044caf1b882791c2ae990788625
Dan Kozlowski <koz@planetscale.com>
2020-03-18 18:30:23 -0700

    Updating vitess image to get Azblob bits
    Signed-off-by: Dan Kozlowski <koz@planetscale.com>

--- pkg/apis/planetscale/v2/defaults.go
+++ pkg/apis/planetscale/v2/defaults.go
@@ -88,7 +88,7 @@ const (
 	// DefaultMysqlPortName is the name for the MySQL port.
 	DefaultMysqlPortName = "mysql"
 
-	defaultVitessLiteImage = "us.gcr.io/planetscale-vitess/lite:2020-03-07.f569401b"
+	defaultVitessLiteImage = "us.gcr.io/planetscale-vitess/lite:2020-03-19.771aa75d"
 )
 
 /*
