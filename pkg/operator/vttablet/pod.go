046c4c8ea6222cede5e03d070871553db9c21bd7
Peter Farr <Peter@PrismaPhonic.com>
2020-05-05 18:16:05 -0700

    Reverting pod changes.
    Signed-off-by: Peter Farr <Peter@PrismaPhonic.com>

--- pkg/operator/vttablet/pod.go
+++ pkg/operator/vttablet/pod.go
@@ -61,8 +61,6 @@ func NewPod(key client.ObjectKey, spec *Spec) *corev1.Pod {
 func UpdatePodInPlace(obj *corev1.Pod, spec *Spec) {
 	// Update labels and annotations, but ignore existing ones we don't set.
 	update.Labels(&obj.Labels, spec.Labels)
-
-	updatePodAnnotations(obj, spec)
 }
 
 // UpdatePod updates all parts of a vttablet Pod to match the desired state,
@@ -75,8 +73,15 @@ func UpdatePod(obj *corev1.Pod, spec *Spec) {
 
 	// Update desired user labels.
 	update.Labels(&obj.Labels, spec.ExtraLabels)
+	// Update desired annotations.
+	update.Annotations(&obj.Annotations, spec.Annotations)
 
-	updatePodAnnotations(obj, spec)
+	// Record hashes of desired label and annotation keys to force the Pod
+	// to be recreated if a key disappears from the desired list.
+	update.Annotations(&obj.Annotations, map[string]string{
+		"planetscale.com/labels-keys-hash":      contenthash.StringMapKeys(spec.ExtraLabels),
+		"planetscale.com/annotations-keys-hash": contenthash.StringMapKeys(spec.Annotations),
+	})
 
 	// Collect some common values that will be shared across containers.
 	volumeMounts := tabletVolumeMounts.Get(spec)
@@ -331,18 +336,6 @@ func UpdatePod(obj *corev1.Pod, spec *Spec) {
 
 	// Use the PriorityClass we defined for vttablets in deploy/priority.yaml.
 	obj.Spec.PriorityClassName = vttabletPriorityClassName
-}
-
-func updatePodAnnotations(obj *corev1.Pod, spec *Spec) {
-	// Update desired annotations.
-	update.Annotations(&obj.Annotations, spec.Annotations)
-
-	// Record hashes of desired label and annotation keys to force the Pod
-	// to be recreated if a key disappears from the desired list.
-	update.Annotations(&obj.Annotations, map[string]string{
-		"planetscale.com/labels-keys-hash":      contenthash.StringMapKeys(spec.ExtraLabels),
-		"planetscale.com/annotations-keys-hash": contenthash.StringMapKeys(spec.Annotations),
-	})
 }
 
 // AliasFromPod returns a TabletAlias corresponding to a vttablet Pod.
