4489e5c50a1d63daaa5d3bfca7c1f2ca17633841
Peter Farr <Peter@PrismaPhonic.com>
2020-09-03 17:14:02 -0700

    Significant refactoring to reconcile handler per review suggestions.
    Signed-off-by: Peter Farr <Peter@PrismaPhonic.com>

--- pkg/controller/vitesskeyspace/reconcile_resharding.go
+++ pkg/controller/vitesskeyspace/reconcile_resharding.go
@@ -19,10 +19,12 @@ package vitesskeyspace
 import (
 	"context"
 	"fmt"
+	"reflect"
 	"sort"
 	"strings"
 
 	corev1 "k8s.io/api/core/v1"
+	"vitess.io/vitess/go/vt/wrangler"
 
 	planetscalev2 "planetscale.dev/vitess-operator/pkg/apis/planetscale/v2"
 )
@@ -37,75 +39,80 @@ func (r *reconcileHandler) reconcileResharding(ctx context.Context) error {
 		return err
 	}
 
-	reshardingInProgress := corev1.ConditionUnknown
-	var workflowStatus *planetscalev2.ReshardingStatus
+	// Look for a resharding workflow. If we find a second one bail out.
+	var reshardingWorkflow *wrangler.ReplicationStatusResult
 	for _, workflowName := range workflowList {
 		workflow, err := r.wr.ShowWorkflow(ctx, workflowName, r.vtk.Spec.Name)
 		if err != nil {
 			return err
 		}
-		if workflow.SourceLocation.Keyspace != workflow.TargetLocation.Keyspace {
-			if reshardingInProgress == corev1.ConditionUnknown {
-				reshardingInProgress = corev1.ConditionFalse
-			}
+		if workflow.SourceLocation.Keyspace != workflow.TargetLocation.Keyspace ||
+			reflect.DeepEqual(workflow.SourceLocation.Shards, workflow.TargetLocation.Shards) {
+			// If keyspaces are not the same we are not resharding. Likewise if keyspaces are the same but shards are identical,
+			// we are also not resharding. Skip this workflow as it's not a resharding related vreplication workflow.
 			continue
 		}
-		reshardingInProgress = corev1.ConditionTrue
 
-		if workflowStatus != nil {
+		if reshardingWorkflow != nil {
+			r.setConditionStatus(planetscalev2.VitessKeyspaceReshardingActive, corev1.ConditionTrue, "MoreThanOneActiveReshardingWorkflow", "Resharding is active, however there is currently more than one active resharding workflow.")
+			r.setConditionStatus(planetscalev2.VitessKeyspaceReshardingInSync, corev1.ConditionUnknown, "MoreThanOneActiveReshardingWorkflow", "More than one resharding workflow. Can't determine which one follow for determining whether we are in sync or not.")
 			return fmt.Errorf("found more than one active resharding workflow")
 		}
 
-		workflowStatus = &planetscalev2.ReshardingStatus{
-			Workflow:     workflow.Workflow,
-			State:        planetscalev2.WorkflowUnknown,
-			SourceShards: workflow.SourceLocation.Shards,
-			TargetShards: workflow.TargetLocation.Shards,
-		}
-
-		// We aggregate status across all the shards for the workflow so we can definitely know if we are in two states:
-		// Copying, or Error. We also do this so we can determine what all of the serving shards are.
-		// At a high level we mostly need to know if we are still in the Copying phase (for any shard whatsoever), or if
-		// we have an error in resharding somewhere that needs to be surfaced.
-		for name, status := range workflow.ShardStatuses {
-			if status.MasterIsServing {
-				shard := strings.Split(name, "/")[0]
-				workflowStatus.ServingShards = append(workflowStatus.ServingShards, shard)
-			}
-			for _, vReplRow := range status.MasterReplicationStatuses {
-				if vReplRow.State == "Error" {
-					workflowStatus.State = planetscalev2.WorkflowError
-					r.vtk.Status.SetConditionStatus(planetscalev2.VitessKeyspaceReshardingInSync, corev1.ConditionFalse, "VReplicationError", fmt.Sprintf("Encountered a VReplication Error: %v", vReplRow.Message))
-					break
-				}
-				if vReplRow.State == "Copying" {
-					workflowStatus.State = planetscalev2.WorkflowCopying
-				}
-				if (vReplRow.State == "Running" || vReplRow.State == "Lagging") && workflowStatus.State == planetscalev2.WorkflowUnknown {
-					workflowStatus.State = planetscalev2.WorkflowRunning
-				}
-			}
-		}
-		if workflowStatus.State == planetscalev2.WorkflowCopying {
-			r.vtk.Status.SetConditionStatus(planetscalev2.VitessKeyspaceReshardingInSync, corev1.ConditionFalse, "WorkflowCopying", fmt.Sprintf("Workflow %v is currently in Copy phase.", workflowStatus.Workflow))
-		}
-
-		// If MaxVReplicationLag ever exceeds max safe value, we need update our condition object.
-		if workflow.MaxVReplicationLag >= maxSafeVReplicationLag {
-			r.vtk.Status.SetConditionStatus(planetscalev2.VitessKeyspaceReshardingInSync, corev1.ConditionFalse, "WorkflowLagging", fmt.Sprintf("Workflow %v is currently lagging by greater than 10 seconds.", workflowStatus.Workflow))
-		}
-
-		sort.Strings(workflowStatus.ServingShards)
+		reshardingWorkflow = workflow
+		break
 	}
 
-	if reshardingInProgress == corev1.ConditionTrue {
-		r.vtk.Status.SetConditionStatus(planetscalev2.VitessKeyspaceReshardingActive, reshardingInProgress, "ReshardingActive", "At least one workflow has active resharding ongoing.")
-	} else if reshardingInProgress == corev1.ConditionFalse {
-		r.vtk.Status.SetConditionStatus(planetscalev2.VitessKeyspaceReshardingActive, reshardingInProgress, "ReshardingInactive", "Active workflows were found, but none that had source and target location involving the same keyspace.")
-	} else {
-		// ConditionUnknown so likely we have no active workflows.
-		r.vtk.Status.SetConditionStatus(planetscalev2.VitessKeyspaceReshardingActive, reshardingInProgress, "NoActiveWorkflows", "No active workflows.")
+	if reshardingWorkflow == nil {
+		r.setConditionStatus(planetscalev2.VitessKeyspaceReshardingActive, corev1.ConditionFalse, "NoActiveReshardingWorkflows", "No active resharding workflows found.")
+		return nil
 	}
+	r.setConditionStatus(planetscalev2.VitessKeyspaceReshardingActive, corev1.ConditionTrue, "ReshardingActive", "One active resharding workflow was found.")
+
+	workflowStatus := &planetscalev2.ReshardingStatus{
+		Workflow:     reshardingWorkflow.Workflow,
+		State:        planetscalev2.WorkflowUnknown,
+		SourceShards: reshardingWorkflow.SourceLocation.Shards,
+		TargetShards: reshardingWorkflow.TargetLocation.Shards,
+	}
+
+	// We aggregate status across all the shards for the workflow so we can definitely know if we are in two states:
+	// Copying, or Error. We also do this so we can determine what all of the serving shards are.
+	// At a high level we mostly need to know if we are still in the Copying phase (for any shard whatsoever), or if
+	// we have an error in resharding somewhere that needs to be surfaced.
+	for name, status := range reshardingWorkflow.ShardStatuses {
+		if status.MasterIsServing {
+			shard := strings.Split(name, "/")[0]
+			workflowStatus.ServingShards = append(workflowStatus.ServingShards, shard)
+		}
+		if workflowStatus.State == planetscalev2.WorkflowError {
+			continue
+		}
+		for _, vReplRow := range status.MasterReplicationStatuses {
+			if vReplRow.State == "Error" {
+				workflowStatus.State = planetscalev2.WorkflowError
+				r.setConditionStatus(planetscalev2.VitessKeyspaceReshardingInSync, corev1.ConditionFalse, "VReplicationError", fmt.Sprintf("Encountered a VReplication Error: %v", vReplRow.Message))
+				break
+			}
+			if vReplRow.State == "Copying" {
+				workflowStatus.State = planetscalev2.WorkflowCopying
+			}
+			if (vReplRow.State == "Running" || vReplRow.State == "Lagging") && workflowStatus.State == planetscalev2.WorkflowUnknown {
+				workflowStatus.State = planetscalev2.WorkflowRunning
+			}
+		}
+	}
+	if workflowStatus.State == planetscalev2.WorkflowCopying {
+		r.setConditionStatus(planetscalev2.VitessKeyspaceReshardingInSync, corev1.ConditionFalse, "WorkflowCopying", fmt.Sprintf("Workflow %v is currently in Copy phase.", workflowStatus.Workflow))
+	}
+
+	// If MaxVReplicationLag ever exceeds max safe value, we need update our condition object.
+	// Copy phase should take precedence though. We don't care about vrepl lag if we are still in copy phase. Regardless we don't allow switching traffic.
+	if reshardingWorkflow.MaxVReplicationLag >= maxSafeVReplicationLag && workflowStatus.State == planetscalev2.WorkflowRunning {
+		r.setConditionStatus(planetscalev2.VitessKeyspaceReshardingInSync, corev1.ConditionFalse, "WorkflowLagging", fmt.Sprintf("Workflow %v is currently lagging by greater than 10 seconds.", workflowStatus.Workflow))
+	}
+
+	sort.Strings(workflowStatus.ServingShards)
 
 	return nil
 }
