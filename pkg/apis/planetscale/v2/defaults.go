0989e36b3458c66a1357f924a5b173fe450f1639
Alkin Tezuysal <alkin.tezuysal@gmail.com>
2021-04-29 20:35:07 +0300

    Update Vitess Dependency v10.0.0 (#193)
    * Update Vitess Dependency v10.0.0
    
    Signed-off-by: Alkin Tezuysal <alkin.tezuysal@gmail.com>
    
    * Update Default Vitess Image v10.0.0
    
    Signed-off-by: Alkin Tezuysal <alkin.tezuysal@gmail.com>

--- pkg/apis/planetscale/v2/defaults.go
+++ pkg/apis/planetscale/v2/defaults.go
@@ -97,7 +97,7 @@ const (
 	// DefaultMysqlPortName is the name for the MySQL port.
 	DefaultMysqlPortName = "mysql"
 
-	defaultVitessLiteImage = "vitess/lite:v10.0.0-rc1"
+	defaultVitessLiteImage = "vitess/lite:v10.0.0"
 )
 
 // DefaultImages are a set of images to use when the CRD doesn't specify.
