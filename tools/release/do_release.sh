e688b985916867c6346a6fccb9c25932dc28631a
Florent Poinsard <florent.poinsard@outlook.fr>
2023-03-02 14:09:06 +0200

    do not update the operator version in operator.yaml during release
    Signed-off-by: Florent Poinsard <florent.poinsard@outlook.fr>

--- tools/release/do_release.sh
+++ tools/release/do_release.sh
@@ -26,23 +26,14 @@ fi
 function updateVitessImages() {
   old_vitess_version=$1
   new_vitess_version=$2
-  new_operator_version=$3
 
   operator_files=$(find -E $ROOT/test/endtoend/operator/* -name "*.yaml" | grep -v "101_initial_cluster.yaml")
   sed -i.bak -E "s/vitess\/lite:(.*)/vitess\/lite:v$new_vitess_version/g" $operator_files
   sed -i.bak -E "s/vitess\/vtadmin:(.*)/vitess\/vtadmin:v$new_vitess_version/g" $operator_files
   sed -i.bak -E "s/vitess\/lite:(.*)/vitess\/lite:v$new_vitess_version\"/g" $ROOT/pkg/apis/planetscale/v2/defaults.go
   sed -i.bak -E "s/vitess\/lite:(.*)/vitess\/lite:v$old_vitess_version/g" $ROOT/test/endtoend/operator/101_initial_cluster.yaml
-  sed -i.bak -E "s/planetscale\/vitess-operator:(.*)/planetscale\/vitess-operator:v$new_operator_version/g" $ROOT/test/endtoend/operator/operator.yaml
 
-  rm -f $(find -E $ROOT/test/endtoend/operator/ -name "*.yaml.bak") $ROOT/pkg/apis/planetscale/v2/defaults.go.bak $ROOT/test/endtoend/operator/operator.yaml.bak
-}
-
-function updateOperatorYaml() {
-  new_operator_version=$1
-
-  sed -i.bak -E "s/planetscale\/vitess-operator:(.*)/planetscale\/vitess-operator:v$new_operator_version/g" "$ROOT/deploy/operator.yaml"
-  rm -f $ROOT/deploy/operator.yaml.bak
+  rm -f $(find -E $ROOT/test/endtoend/operator/ -name "*.yaml.bak") $ROOT/pkg/apis/planetscale/v2/defaults.go.bak
 }
 
 function updateVersion() {
@@ -63,7 +54,6 @@ fi
 
 updateVersion $NEW_OPERATOR_VERSION
 updateVitessImages $OLD_VITESS_VERSION $NEW_VITESS_VERSION $NEW_OPERATOR_VERSION
-updateOperatorYaml $NEW_OPERATOR_VERSION
 
 git add --all
 git commit -n -s -m "Release commit for $NEW_OPERATOR_VERSION"
