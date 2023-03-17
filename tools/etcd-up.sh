9880361fd3798304e611e63d571bb5507cf38c27
Jack Carrig <jack.carrig@clover.com>
2023-03-17 11:24:54 -0400

    add script to start etcd
    Signed-off-by: Jack Carrig <jack.carrig@clover.com>

---
+++ tools/etcd-up.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+ETCD_PORT=2379
+ETCD_URL="http://127.0.0.1:${ETCD_PORT}"
+
+
+function fail() {
+  echo "ERROR: $1"
+  exit 1
+}
+
+curl "${ETCD_URL}" > /dev/null 2>&1 && fail "etcd is already running. Exiting."
+
+echo "starting etcd on ${ETCD_URL}"
+
+etcd \
+    --data-dir /vtop/etcd/ \
+    --listen-client-urls "${ETCD_URL}" \
+    --advertise-client-urls "${ETCD_URL}" \
+    --listen-peer-urls http://127.0.0.1:0 \
+    > /vtop/tmp/etcd.out 2>&1 &
+PID=$!
+echo $PID > /vtop/tmp/etcd.pid
+sleep 5
+
+echo "etcd is up"
