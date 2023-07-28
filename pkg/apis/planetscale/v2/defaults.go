42ca5bc70c121c659c331d50a104cae06c1dce9b
Matt Lord <mattalord@gmail.com>
2023-07-28 10:04:38 -0400

    Release commit for 2.8.5
    Signed-off-by: Matt Lord <mattalord@gmail.com>

--- pkg/apis/planetscale/v2/defaults.go
+++ pkg/apis/planetscale/v2/defaults.go
@@ -102,7 +102,7 @@ const (
 	// DefaultMysqlPortName is the name for the MySQL port.
 	DefaultMysqlPortName = "mysql"
 
-	defaultVitessLiteImage = "vitess/lite:v15.0.3"
+	defaultVitessLiteImage = "vitess/lite:v15.0.4"
 )
 
 // DefaultImages are a set of images to use when the CRD doesn't specify.
