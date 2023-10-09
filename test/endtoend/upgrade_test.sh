9ddcf14fd6de403b1729a19fa481bb7af845ceff
Matt Lord <mattalord@gmail.com>
2023-10-09 14:59:43 -0400

    Update verifyVtGateVersion
    Signed-off-by: Matt Lord <mattalord@gmail.com>

--- test/endtoend/upgrade_test.sh
+++ test/endtoend/upgrade_test.sh
@@ -250,7 +250,7 @@ checkSemiSyncSetup
 # Initially too durability policy should be specified
 verifyDurabilityPolicy "commerce" "semi_sync"
 upgradeToLatest
-verifyVtGateVersion "17.0.2"
+verifyVtGateVersion "17.0.3"
 checkSemiSyncSetup
 # After upgrading, we verify that the durability policy is still semi_sync
 verifyDurabilityPolicy "commerce" "semi_sync"
