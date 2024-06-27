f7e94cec798cfe180d9cd443190e743543fa5229
Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>
2024-06-27 08:21:09 +0300

    update to 20.0.0
    Signed-off-by: Shlomi Noach <2607934+shlomi-noach@users.noreply.github.com>

--- test/endtoend/vtorc_vtadmin_test.sh
+++ test/endtoend/vtorc_vtadmin_test.sh
@@ -242,7 +242,7 @@ killall kubectl
 setupKubectlAccessForCI
 
 get_started_vtorc_vtadmin
-verifyVtGateVersion "20.0.0-rc2"
+verifyVtGateVersion "20.0.0"
 checkSemiSyncSetup
 
 # Check Vtadmin is setup
