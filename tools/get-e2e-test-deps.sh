f40db0726a297bd5807ba05cb1fb122d72b0fda4
Florent Poinsard <florent.poinsard@outlook.fr>
2024-07-23 10:35:56 -0600

    Fix vitess sha
    Signed-off-by: Florent Poinsard <florent.poinsard@outlook.fr>

--- tools/get-e2e-test-deps.sh
+++ tools/get-e2e-test-deps.sh
@@ -41,7 +41,7 @@ if ! command -v vtctldclient &> /dev/null
 then
   echo "Downloading vtctldclient..."
   version=20.0.1
-  file=vitess-${version}-7e8c974.tar.gz
+  file=vitess-${version}-003c441.tar.gz
   wget https://github.com/vitessio/vitess/releases/download/v${version}/${file}
   tar -xzf ${file}
   cd ${file/.tar.gz/}
