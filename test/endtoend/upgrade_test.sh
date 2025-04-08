a0ca5bb2444455852851e32b5cb297364ba7cdba
Florent Poinsard <florent.poinsard@outlook.fr>
2025-04-08 09:49:28 -0600

    use v22 as target vitess version in tests
    Signed-off-by: Florent Poinsard <florent.poinsard@outlook.fr>

--- test/endtoend/upgrade_test.sh
+++ test/endtoend/upgrade_test.sh
@@ -310,7 +310,7 @@ checkSemiSyncSetup
 # Initially too durability policy should be specified
 verifyDurabilityPolicy "commerce" "semi_sync"
 upgradeToLatest
-verifyVtGateVersion "23.0.0"
+verifyVtGateVersion "22.0.0"
 checkSemiSyncSetup
 # After upgrading, we verify that the durability policy is still semi_sync
 verifyDurabilityPolicy "commerce" "semi_sync"
