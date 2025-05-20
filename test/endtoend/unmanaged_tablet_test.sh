29a5eeb205e9ce0019226d6843734e12a5b85a49
Sergey Stankevich <stankevich@users.noreply.github.com>
2025-05-20 14:06:27 +0200

    Wait for unmanaged MySQL pod to be running
    Signed-off-by: Sergey Stankevich <stankevich@users.noreply.github.com>

--- test/endtoend/unmanaged_tablet_test.sh
+++ test/endtoend/unmanaged_tablet_test.sh
@@ -3,7 +3,6 @@
 source ./tools/test.env
 source ./test/endtoend/utils.sh
 
-# get_started_unmanaged:
 function get_started_unmanaged() {
     echo "Apply latest operator-latest.yaml"
     kubectl apply -f "operator-latest.yaml"
@@ -11,6 +10,8 @@ function get_started_unmanaged() {
 
     echo "Apply 101_initial_cluster_unmanaged_tablet.yaml"
     kubectl apply -f "101_initial_cluster_unmanaged_tablet.yaml"
+    # Wait for the MySQL pod to be running first to avoid race conditions
+    checkPodStatusWithTimeout "mysql-(.*)1/1(.*)Running(.*)" 1
     checkPodStatusWithTimeout "example-zone1-vtctld(.*)1/1(.*)Running(.*)"
     checkPodStatusWithTimeout "example-zone1-vtgate(.*)1/1(.*)Running(.*)"
     checkPodStatusWithTimeout "example-etcd(.*)1/1(.*)Running(.*)" 3
